35b3f5ab4955be8c4eb5118e3e2448fb9179871c
ci parkin <booshbrew@gmail.com>
2022-04-03 16:50:33 +0100

    commit

--- script.js
+++ script.js
@@ -1,63 +1,12 @@
-var colors = function () {
-    console.log("red", "green", "blue");
-    
-    }
-    colors();
+var time = document.querySelector("time")
+var timeSeconds = 15
 
+startButton.addEventListener("click", startGame);
 
-    console.log("hello")
+function startGame() {
 
+    // Start the Timer
+    startTimer()
 
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-start.btn.addEventListener("click", function(event){
-
-}
-
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-// list of all questions, choices, and answers
-var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
-    },
-    {
-      title:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    }
-  ];
-  
+//<div class="timer">Time: <span id="time">10</span></div>
+//<button id="start" id="onclick">Start Quiz</button>
