d55ba972795ccda8af9fcc90a0ef81ddbf40fdb1
ci parkin <booshbrew@gmail.com>
2022-04-10 14:51:17 +0100

    commit

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
-// var timerEl = document.getElementById("timer");
-// var startButton = document.getElementById("start");
-// var timeleft = 15; 
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("time");
+var timeleft; 
 
 
-// function startTimer() {
+function startTimer() {
     
-//     //Here goes the code for that we want startTimer to actually do
+    //Here goes the code for that we want startTimer to actually do
    
-//     var timerEl = setInterval(startTimer, 1000);
-//   if ()
-
-// }
-
-
-// startButton.addEventListener("click", startTimer)
-
-
-var timerEl = document.getElementById('start');
-var mainEl = document.getElementById('main');
-var startButton = document.getElementById("start");
-
-var message =
-  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-var words = message.split(' ');
-
-
-
-function countdown() {
-    var timeLeft = 15;
+    var timer = setInterval(startTimer, 1000);
   
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        displayMessage();
-      }
-    }, 1000);
-  }
-  startButton.addEventListener("click", startTimer)
+      // Sets timer
+      timer = setInterval(function() {
+      
+        if (timerCount >= 0) {
+          // Tests if win condition is met
+          if (isWin && timerCount > 0) {
+            // Clears interval and stops timer
+            clearInterval(timer);
+            winGame();
+          }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+          // Clears interval
+          clearInterval(timer);
+          loseGame();
+        }
+      }, 1000);
+
+}
+
+
+startButton.addEventListener("click", startTimer)
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+
+
+
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+// ```
