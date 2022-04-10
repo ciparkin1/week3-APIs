bb984cf108f15b33289d529d7bf2dc9e4e0a7682
ci parkin <booshbrew@gmail.com>
2022-04-10 11:42:01 +0100

    commit

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var time = document.querySelector("time")
-var timeSeconds = 15
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var timeleft = 15; 
 
-startButton.addEventListener("click", startGame);
 
-setInterval(startTime, 1000);
+function startTimer() {
+    
+    //Here goes the code for that we want startTimer to actually do
+   
+    var timerEl = setInterval(startTimer, 1000);
+}
 
-function startGame() {
+startButton.addEventListener("click", startTimer)}
 
-    // Start the Timer
-    startTimer()
-
-//<div class="timer">Time: <span id="time">10</span></div>
-//<button id="start" id="onclick">Start Quiz</button>
+/* <div class="timer">Time: <span id="time">10</span></div>
+<button id="start" id="onclick">Start Quiz</button>*/
